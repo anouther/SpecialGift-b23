12455608e3d53e404ab07cc5f926205b82bb894e
anouther <155570107+anouther@users.noreply.github.com>
2025-05-13 20:26:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "happy mensiversary yang ke 6 sayangku, makasih ya udah pilih buat stay sama aku disini. aku ga pernah nyesel buat jadi pacar kamu, i love you sayangku ";
+            subtitle.innerText = "Another month, another chapter of us. Not always perfect, not always easy but always real. Thank you for growing with me, laughing with me, and choosing me, even on the days i forget how to love myself. You're not just my person but you're my peace. Thank you for always being by my side and always being the home i come home to. You're mean so much for me, i'm better with you kaak. i know you love brown so i bought this flower for youuu =33 ";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
